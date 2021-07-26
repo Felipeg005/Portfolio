935a1e58cffe6f2dec659ac231ce0ec3a262aa18
AlexHamn <agalexandro16@gmail.com>
2021-07-26 17:00:40 -0500

    add functioning js

--- ham-menu.js
+++
@@ -1,3 +0,0 @@
-let hamburgerButton = document.querySelector("Hamb-button");
-
-
---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburgerButton = document.querySelector(".header");
+
+hamburgerButton.addEventListener("click", e => {
+  console.log(e);
+});
