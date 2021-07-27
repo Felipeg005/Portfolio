6cf4c1c455a4b1b40cbb241501935a212d8dc4d4
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-07-26 20:01:06 -0500

    add styles and functions for hamburger menu

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const hamburgerButton = document.querySelector(".header");
+const header = document.querySelector(".header");
+const headerelement1 = document.querySelector(".headerelement1");
+const navMenu = document.querySelector(".headerelement2");
+const menuItems = document.querySelector(".menuitems");
+const lines = document.querySelector(".lines");
 
-hamburgerButton.addEventListener("click", e => {
-  console.log(e);
-});
+
+
+header.addEventListener("click", clickMenu)
+
+function clickMenu () {
+  header.classList.toggle("header1");
+  headerelement1.classList.toggle("headerelement1-1");
+  menuItems.classList.toggle("menuitems1");
+  lines.classList.toggle("lines1");
+  navMenu.classList.toggle("nav-menu");
+
+}
