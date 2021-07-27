5f0ccb2928311f77135e61224a06967f96ffa6f8
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-07-27 00:28:52 -0500

    Add transitions and styles for hamburger menu

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const headerelement1 = document.querySelector(".headerelement1");
 const navMenu = document.querySelector(".headerelement2");
 const menuItems = document.querySelector(".menuitems");
 const lines = document.querySelector(".lines");
+const menuLine1 = document.querySelector(".line-1");
+const menuLine2 = document.querySelector(".line-2");
+const menuLine3 = document.querySelector(".line-3");
+const navMenuBottom = document.querySelector(".navmenubottom");
 
 
 
@@ -14,5 +18,8 @@ function clickMenu () {
   menuItems.classList.toggle("menuitems1");
   lines.classList.toggle("lines1");
   navMenu.classList.toggle("nav-menu");
-
+  menuLine1.classList.toggle("line-1-1");
+  menuLine2.classList.toggle("line-1-2");
+  menuLine3.classList.toggle("line-1-3");
+  navMenuBottom.classList.toggle("navmenubottom1");
 }
