4aad61dfc72d0d9ef031cf82dd85c801ca947c8d
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-07-27 00:33:08 -0500

    linters check updates

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-const header = document.querySelector(".header");
-const headerelement1 = document.querySelector(".headerelement1");
-const navMenu = document.querySelector(".headerelement2");
-const menuItems = document.querySelector(".menuitems");
-const lines = document.querySelector(".lines");
-const menuLine1 = document.querySelector(".line-1");
-const menuLine2 = document.querySelector(".line-2");
-const menuLine3 = document.querySelector(".line-3");
-const navMenuBottom = document.querySelector(".navmenubottom");
+const header = document.querySelector('.header');
+const headerelement1 = document.querySelector('.headerelement1');
+const navMenu = document.querySelector('.headerelement2');
+const menuItems = document.querySelector('.menuitems');
+const lines = document.querySelector('.lines');
+const menuLine1 = document.querySelector('.line-1');
+const menuLine2 = document.querySelector('.line-2');
+const menuLine3 = document.querySelector('.line-3');
+const navMenuBottom = document.querySelector('.navmenubottom');
 
+header.addEventListener('click', clickMenu);
 
-
-header.addEventListener("click", clickMenu)
-
-function clickMenu () {
-  header.classList.toggle("header1");
-  headerelement1.classList.toggle("headerelement1-1");
-  menuItems.classList.toggle("menuitems1");
-  lines.classList.toggle("lines1");
-  navMenu.classList.toggle("nav-menu");
-  menuLine1.classList.toggle("line-1-1");
-  menuLine2.classList.toggle("line-1-2");
-  menuLine3.classList.toggle("line-1-3");
-  navMenuBottom.classList.toggle("navmenubottom1");
+function clickMenu() {
+  header.classList.toggle('header1');
+  headerelement1.classList.toggle('headerelement1-1');
+  menuItems.classList.toggle('menuitems1');
+  lines.classList.toggle('lines1');
+  navMenu.classList.toggle('nav-menu');
+  menuLine1.classList.toggle('line-1-1');
+  menuLine2.classList.toggle('line-1-2');
+  menuLine3.classList.toggle('line-1-3');
+  navMenuBottom.classList.toggle('navmenubottom1');
 }
